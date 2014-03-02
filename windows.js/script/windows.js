adeff2f719bbd8c40deab4d1c3f2278611f4e541
Andy James <me@andyjames.it>
2014-03-02 02:18:21 +0000

    Added new CSS for better handling of new cursor for movable windows, added modal window creation / handling.

--- windows.js/script/windows.js
+++ windows.js/script/windows.js
@@ -37,11 +37,12 @@ var windows = {
     },
     maxedWindows: [],
     hiddenWindows: [],
+    modalWindows: [],
     allWindows: [],
     load: function (settings) {
         $.extend(windows.settings, settings);
         windows.settings.enclosure = $(windows.settings.enclosure);
-        windows.settings.enclosure.addClass("win-js-enclosure");
+        windows.settings.enclosure.addClass('win-js-enclosure');
         $(window).mouseup(windows.internal.fn.globalMouseUp);
         $(window).mousemove(windows.internal.fn.globalMouseMove);
         $(window).resize(windows.internal.fn.globalResize);
@@ -51,37 +52,47 @@ var windows = {
         $.extend(ws, windows.presets.default);
         $.extend(ws, settings);
         if (enclosure == null) enclosure = windows.settings.enclosure;
-        var win, tb, tb_title, tb_hide, tb_maximise, tb_close, tb_icon;
-        win = $('<div/>', { class: 'window', id: windows.internal.guid() });
+        var win, modal, tb, tb_title, tb_staters, tb_hide, tb_state, tb_close, tb_icon;
+        win = $('<div/>', { class: 'win-js-window', id: windows.internal.guid() });
         win.css({ top: ws.top, left: ws.left, width: ws.width, height: ws.height });
-        tb = $('<div/>', { class: 'tb' }).appendTo(win);
-        if (ws.icon) tb_icon = $('<img/>', { class: 'icon', src: ws.icon }).appendTo(tb);
-        if (ws.close) tb_close = $('<div/>', { class: 'close' }).appendTo(tb);
-        if (ws.state) tb_maximise = $('<div/>', { class: 'state' }).appendTo(tb);
-        if (ws.hide) tb_hide = $('<div/>', { class: 'hide' }).appendTo(tb);
-        tb_title = $('<div/>', { class: 'title', text: ws.title }).appendTo(tb);
-        $('<div/>', { class: 'clear' }).appendTo(tb);
-        $('<div/>', { class: 'content' }).appendTo(win);
+        if (ws.movable) win.addClass('win-js-movable');
+        tb = $('<div/>', { class: 'win-js-tb' }).appendTo(win);
+        if (ws.icon) tb_icon = $('<img/>', { class: 'win-js-icon', src: ws.icon }).appendTo(tb);
+        if (ws.close || ws.state || ws.hide) tb_staters = $('<div/>', { class: 'win-js-staters' }).appendTo(tb);
+        if (ws.hide) tb_hide = $('<div/>', { class: 'win-js-hide' }).appendTo(tb_staters);
+        if (ws.state) tb_state = $('<div/>', { class: 'win-js-state' }).appendTo(tb_staters);
+        if (ws.close) tb_close = $('<div/>', { class: 'win-js-close' }).appendTo(tb_staters);
+        tb_title = $('<div/>', { class: 'win-js-title', text: ws.title }).appendTo(tb);
+        $('<div/>', { class: 'win-js-clear' }).appendTo(tb);
+        $('<div/>', { class: 'win-js-content' }).appendTo(win);
         if (ws.close) tb_close.click(windows.internal.fn.wintbCloseClick);
         if (ws.close && ws.icon) tb_icon.dblclick(windows.internal.fn.wintbCloseClick);
-        if (ws.state) tb_maximise.click(windows.internal.fn.wintbMaxClick);
+        if (ws.state) tb_state.click(windows.internal.fn.wintbMaxClick);
         if (ws.state) tb_title.dblclick(windows.internal.fn.wintbMaxClick);
         if (ws.hide) tb_hide.click(windows.internal.fn.wintbHideClick);
-        if (ws.movable) tb.mousedown(windows.internal.fn.wintbMouseDown);
+        if (ws.movable) tb_title.mousedown(windows.internal.fn.wintbMouseDown);
         win.mousedown(windows.internal.fn.winMouseDown);
-        win.hide().appendTo(enclosure);
+        if (ws.modal) {
+            modal = $('<div/>', { class: 'win-js-modal' });
+            win.hide().appendTo(modal);
+            modal.appendTo(enclosure);
+            windows.bringToFront(modal);
+            windows.modalWindows.push(win);
+        } else {
+            win.hide().appendTo(enclosure);
+            windows.bringToFront(win);
+        }
         windows.allWindows.push(win);
-        windows.bringToFront(win);
         win.fadeIn(windows.settings.speed);
         return win;
     },
     max: function (win) {
         if (windows.isMaxedWindow(win)) return;
-        win.addClass("maxed");
-        win.data("res-t", win.offset().top);
-        win.data("res-l", win.offset().left);
-        win.data("res-w", win.width());
-        win.data("res-h", win.height());
+        win.addClass('win-js-maxed');
+        win.data('res-t', win.offset().top);
+        win.data('res-l', win.offset().left);
+        win.data('res-w', win.width());
+        win.data('res-h', win.height());
         windows.internal.animateToMax(win);
         if (!windows.isMaxedWindow(win))
             windows.maxedWindows.push(win);
@@ -89,37 +100,47 @@ var windows = {
     res: function (win) {
         if (!windows.isMaxedWindow(win)) return;
         windows.internal.removeMaxedWindow(win);
-        win.removeClass("maxed");
+        win.removeClass('win-js-maxed');
         win.stop();
         win.animate({
-            top: win.data("res-t"),
-            left: win.data("res-l"),
-            width: win.data("res-w"),
-            height: win.data("res-h")
+            top: win.data('res-t'),
+            left: win.data('res-l'),
+            width: win.data('res-w'),
+            height: win.data('res-h')
         }, windows.settings.speed);
-        win.data("res-t", null);
-        win.data("res-l", null);
-        win.data("res-w", null);
-        win.data("res-h", null);
+        win.data('res-t', null);
+        win.data('res-l', null);
+        win.data('res-w', null);
+        win.data('res-h', null);
     },
     resTo: function (win, css) {
         if (!windows.isMaxedWindow(win)) return;
         windows.internal.removeMaxedWindow(win);
-        win.removeClass("maxed");
+        win.removeClass('win-js-maxed');
         win.stop();
         win.animate(css, windows.settings.speed);
-        win.data("res-t", null);
-        win.data("res-l", null);
-        win.data("res-w", null);
-        win.data("res-h", null);
+        win.data('res-t', null);
+        win.data('res-l', null);
+        win.data('res-w', null);
+        win.data('res-h', null);
     },
     hide: function (win) {
         if (windows.isHiddenWindow(win)) return;
-        win.fadeOut(windows.settings.speed);
+        win.fadeOut(
+            windows.settings.speed,
+            function () {
+                if (windows.isModalWindow(win)) {
+                    win.parents('.win-js-modal').hide();
+                }
+            });
+        
         windows.hiddenWindows.push(win);
     },
     show: function (win) {
         if (!windows.isHiddenWindow(win)) return;
+        if (windows.isModalWindow(win)) {
+            win.parents('.win-js-modal').show();
+        }
         win.fadeIn(windows.settings.speed);
         windows.internal.removeHiddenWindow(win);
     },
@@ -127,11 +148,25 @@ var windows = {
         win.fadeOut(
             windows.settings.speed,
             function () {
-                $(this).remove();
-                windows.internal.removeWindow($(this));
+                if (windows.isHiddenWindow(win)) {
+                    windows.internal.removeHiddenWindow(win);
+                }
+                if (windows.isMaxedWindow(win)) {
+                    windows.internal.removeMaxedWindow(win);
+                }
+                if (windows.isModalWindow(win)) {
+                    win.parents('.win-js-modal').remove();
+                    windows.internal.removeModalWindow(win);
+                } else {
+                    win.remove();
+                }
+                windows.internal.removeWindow(win);
             });
     },
     bringToFront: function (win) {
+        if (windows.isModalWindow(win)) {
+            win = win.parents('.win-js-modal');
+        }
         var winIndex = win.css('z-index') - 1;
         $(windows.allWindows).each(function (i) {
             var curIndex = $(this).css('z-index');
@@ -152,16 +187,22 @@ var windows = {
                 return true;
         return false;
     },
+    isModalWindow: function (win) {
+        for (var i = 0; i < windows.modalWindows.length; i++)
+            if (windows.modalWindows[i].is(win))
+                return true;
+        return false;
+    },
     internal: {
         animateToMax: function (win) {
             win.stop();
             win.animate({
                 top: windows.settings.enclosure.offset().top,
                 left: windows.settings.enclosure.offset().left,
-                width: windows.settings.enclosure.width() - parseInt(win.css("border-left-width")) - parseInt(win.css("border-right-width"))
-                - parseInt(win.css("padding-left")) - parseInt(win.css("padding-right")),
-                height: windows.settings.enclosure.height() - parseInt(win.css("border-top-width")) - parseInt(win.css("border-bottom-width"))
-                - parseInt(win.css("padding-top")) - parseInt(win.css("padding-top")),
+                width: windows.settings.enclosure.width() - parseInt(win.css('border-left-width')) - parseInt(win.css('border-right-width'))
+                - parseInt(win.css('padding-left')) - parseInt(win.css('padding-right')),
+                height: windows.settings.enclosure.height() - parseInt(win.css('border-top-width')) - parseInt(win.css('border-bottom-width'))
+                - parseInt(win.css('padding-top')) - parseInt(win.css('padding-top')),
             }, windows.settings.speed);
         },
         removeMaxedWindow: function (win) {
@@ -173,6 +214,11 @@ var windows = {
             for (var i = 0; i < windows.hiddenWindows.length; i++)
                 if (windows.hiddenWindows[i].is(win))
                     windows.hiddenWindows.splice(i, 1);
+        },
+        removeModalWindow: function (win) {
+            for (var i = 0; i < windows.modalWindows.length; i++)
+                if (windows.modalWindows[i].is(win))
+                    windows.modalWindows.splice(i, 1);
         },
         removeWindow: function (win) {
             windows.internal.removeMaxedWindow(win);
@@ -188,38 +234,44 @@ var windows = {
         },
         fn: {
             wintbMouseDown: function(e) {
-                var sw = $(this).parents(".window");
-                var swot = sw.offset();
-                windows.internal.cw.cw = sw;
+                var win = $(this).parents('.win-js-window');
+                win.removeClass('win-js-movable');
+                win.addClass('win-js-moving');
+                var swot = win.offset();
+                windows.internal.cw.cw = win;
                 windows.internal.cw.ct = e.pageY - swot.top;
-                if (!windows.isMaxedWindow(sw)) windows.internal.cw.cl = e.pageX - swot.left;
-                else windows.internal.cw.cl = sw.data("res-w") / 2;
+                if (!windows.isMaxedWindow(win)) windows.internal.cw.cl = e.pageX - swot.left;
+                else windows.internal.cw.cl = win.data('res-w') / 2;
             },
             winMouseDown: function(e) {
                 windows.bringToFront($(this));
             },
             wintbCloseClick: function (e) {
-                var win = $(this).parents(".window");
+                var win = $(this).parents('.win-js-window');
                 windows.close(win);
             },
             wintbMaxClick: function (e) {
-                var win = $(this).parents('.window');
+                var win = $(this).parents('.win-js-window');
                 if (windows.isMaxedWindow(win)) windows.res(win);
                 else windows.max(win);
             },
             wintbHideClick: function (e) {
-                var win = $(this).parents(".window");
+                var win = $(this).parents('.win-js-window');
                 windows.hide(win);
             },
             globalMouseUp: function () {
+                if (windows.internal.cw.cw == null) return;
+                var win = windows.internal.cw.cw;
+                win.removeClass('win-js-moving');
+                win.addClass('win-js-movable');
                 windows.internal.cw = {};
             },
             globalMouseMove: function (e) {
                 if (windows.internal.cw.cw == null) return;
                 var win = windows.internal.cw.cw;
                 if (windows.isMaxedWindow(win))
-                    windows.resTo(win, { width: win.data("res-w"),
-                        height: win.data("res-h") });
+                    windows.resTo(win, { width: win.data('res-w'),
+                        height: win.data('res-h') });
                 win.css({
                     top: e.pageY - windows.internal.cw.ct,
                     left: e.pageX - windows.internal.cw.cl,
